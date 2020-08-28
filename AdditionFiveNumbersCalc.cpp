6cb2501c6d9f705099bc5bd83393d11d2182c175
Praneel Mahalatkar <69175954+Angular207@users.noreply.github.com>
2020-08-28 16:58:39 -0500

    Add files via upload

---
+++ AdditionFiveNumbersCalc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int a, b, c, d, e, f;
+    cout << "Welcome to the Five Number Addition Calculator!" << endl;
+    cout << "What is the first number?" << endl;
+    cin >> a;
+    cout << "What is the second number?" << endl;
+    cin >> b;
+    cout << "What is the thrid number?" << endl;
+    cin >> c;
+    cout << "What is the fourth number?" << endl;
+    cin >> d;
+    cout << "What is the fifth number?" << endl;
+    cin >> e;
+    f = a + b + c + d, e;
+    cout << "The answer is " << f << "." << endl;
+    cout << "Thank you for viewing this program!" << endl;
+}
